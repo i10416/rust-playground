5ff7ab18cc03b994454e7684aeacaf97209a2914
ItoYo16u <ito.yo16uh90616@gmail.com>
2021-06-30 00:38:34 +0900

    update io impl

--- src/io_examples/file_io.rs
+++ src/io_examples/file_io.rs
@@ -1,22 +1,36 @@
-
-
 pub mod file_io {
-  use std::io::prelude::*;
-  use std::fs::File;
+    use std::fs::File;
+    use std::io::{prelude::*, BufReader};
 
     use std::path::Path;
-
-  pub fn open(p: &str){
-    let path = Path::new(p);
-    let mut file = match File::open(&path){
-      Err(reason) => panic!("couldn't open {}: {}",path.display(),reason.to_string()),
-      Ok(f) => f,
-    };
-    let mut s = String::new();
-    match file.read_to_string(&mut s) {
-      Err(reason) => panic!("counldn't read {}: {}",path.display(),reason.to_string()),
-      Ok(_) => print!("{} contains:\n{}",path.display(),s), 
-    };
-  }
-  pub fn write(){}
+    // expect: `linecounts    wordcounts    bytecounts    filename\n
+    pub fn wc(p: &str) -> Result<String, String> {
+        let path = Path::new(p);
+        let file = match File::open(path) {
+            Err(reason) => panic!("couldn't open {}: {}", path.display(), reason.to_string()),
+            Ok(f) => f,
+        };
+        let size = count_byte(&file);
+        Ok(fmt(0, size, 0, path.display().to_string()))
+    }
+    // count file byte size
+    fn count_byte(file: &File) -> usize {
+        let b = BufReader::new(file);
+        let size = b.bytes().count();
+        size
+    }
+    fn count_lines(file: &File) -> usize {
+        1
+    }
+    fn count_words(file: &File) -> usize {
+        1
+    }
+    fn fmt(linecount: usize, wordcount: usize, bytecount: usize, filename: String) -> String {
+        let s = format!(
+            "{}    {}    {}    {}\n",
+            linecount, wordcount, bytecount, filename
+        );
+        s
+    }
+    fn write() {}
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use playground::io_examples::file_io::file_io;
+use playground::io_examples::console_io;
+fn main() -> Result<(),String> {
+    // note: specify a path from the project root.
+    
+    // recieve command line args.
+    // args[0] is the name of program.
+    let args:Vec<String> = env::args().collect();
+    let s:&str = &args[1];
+
+    file_io::wc(s).map(|f| print!("{}",f))
 }
