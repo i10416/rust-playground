ff4f8099d5ceadf6c26d3d16c7b52bf7b0b6e0b0
ItoYo16u <ito.yo16uh90616@gmail.com>
2021-06-30 22:00:51 +0900

    fix bug

--- src/io_examples/file_io.rs
+++ src/io_examples/file_io.rs
@@ -13,26 +13,26 @@ pub mod file_io {
 
     fn count_all(path: &Path) -> (usize, usize, usize) {
         let file = File::open(path).unwrap();
-        let buf = vec![];
-        read_to_buffer_rec(BufReader::new(file), buf, (0, 0, 0))
+        let mut buf = String::new();
+        read_to_buffer_rec(BufReader::new(file), &mut buf, (0, 0, 0))
     }
 
     fn read_to_buffer_rec(
         mut reader: BufReader<File>,
-        mut buf: Vec<u8>,
+        mut buf: &mut String,
         acc: (usize, usize, usize),
     ) -> (usize, usize, usize) {
-        match reader.read(&mut buf) {
+        match reader.read_line(&mut buf) {
             // reach the end of the file or file contains 0 byte
             Ok(n) if n <= 0 => (
                 acc.0 + count_bytes(&reader),
-                acc.1 + count_words(&reader),
+                acc.1 + count_words_by_line(&buf),
                 acc.2 + count_line_ends(&reader),
             ),
             Ok(n) => {
-                let ls = count_line_ends(&reader);
-                let ws = count_words(&reader);
-                read_to_buffer_rec(reader, vec![], (acc.0 + n, acc.1 + ws, acc.2 + ls))
+                let ws = count_words_by_line(&buf);
+                let mut b = String::new();
+                read_to_buffer_rec(reader, &mut b, (acc.0 + n, acc.1 + ws, acc.2 + 1))
             }
             Err(_) => acc,
         }
@@ -41,11 +41,12 @@ pub mod file_io {
     fn count_bytes(r: &BufReader<File>) -> usize {
         r.buffer().bytes().count()
     }
+    fn count_words_by_line(s: &String) -> usize {
+        s.split(" ")
+            .fold(0, |acc, word| if word.is_empty() { acc } else { acc + 1 })
+    }
     fn count_line_ends(r: &BufReader<File>) -> usize {
         r.buffer().iter().filter(|b| (**b as char) == '\n').count()
-    }
-    fn count_words(r: &BufReader<File>) -> usize {
-        String::from_utf8_lossy(r.buffer()).split(" ").count()
     }
 
     fn fmt(linecount: usize, wordcount: usize, bytecount: usize, filename: String) -> String {
